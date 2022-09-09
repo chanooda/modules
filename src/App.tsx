6456a4d5cd6f6d841fc735f8aa4297d90baded1b
chanooda <hanrhfqkq@gmail.com>
2022-09-10 03:24:25 +0900

    react hook form module

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,95 @@
-import { IInputs } from './FormHook';
+import React from 'react';
+import { SubmitHandler } from 'react-hook-form';
+import styled from 'styled-components';
+import { IInputs, useCustomForm } from './FormHook';
+
+interface ISignupInput {
+  id: string;
+  name: string;
+  password: string;
+  phone: string;
+  email: string;
+  [key: string]: string;
+}
+
+const inputs: IInputs<ISignupInput>[] = [
+  { name: 'id', label: '아이디', type: 'text', buttonText: '중복 확인' },
+  { name: 'password', label: '비밀번호', type: 'password' },
+  { name: 'passwordCheck', label: '비밀번호 확인', type: 'text' },
+  { name: 'name', label: '이름', type: 'text' },
+  { name: 'phone', label: '전화번호', type: 'text', buttonText: '본인 인증' },
+  { name: 'email', label: '이메일', type: 'text', buttonText: '이메일 인증' },
+];
 
 function App() {
-  const inputs: IInputs[] = [
-    { key: 'id', label: '아이디', type: 'text', isButton: true },
-  ];
+  const onValid: SubmitHandler<ISignupInput> = (formData) => {
+    const { password, passwordConfirm } = formData;
+    if (password !== passwordConfirm) {
+      console.log('비밀번호 같지 않음');
+      return;
+    }
+    console.log(formData);
+  };
 
-  return <div></div>;
+  const customInValid = (errMessage?: string) => {
+    console.log(errMessage);
+  };
+
+  const onClicks = {
+    id() {
+      watch('id');
+      console.log(watch('id'));
+    },
+    phone() {
+      watch('phone');
+      console.log(watch('phone'));
+    },
+    email() {
+      watch('phone');
+      console.log(watch('phone'));
+    },
+  };
+
+  const { input, inputList, onSubmit, watch } = useCustomForm<ISignupInput>(
+    inputs,
+    onValid,
+    customInValid,
+    onClicks
+  );
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <AppContainer>
+          {inputList.map((el, i) => (
+            <React.Fragment key={i}>{el}</React.Fragment>
+          ))}
+          <button>회원가입</button>
+        </AppContainer>
+        <>
+          ============================================================================================
+        </>
+      </form>
+    </>
+  );
 }
+
+const AppContainer = styled.div`
+  margin: 0 auto;
+  padding: 24px;
+  width: 500px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  > button {
+    height: 35px;
+    border: none;
+    background-color: #39a6f0;
+    border-radius: 5px;
+    color: white;
+    font-weight: 700;
+    font-size: 16px;
+  }
+`;
 
 export default App;
--- src/FormHook.tsx
+++ src/FormHook.tsx
@@ -1,49 +1,143 @@
-import { UseFormRegister } from 'react-hook-form/dist/types';
-import { useForm } from 'react-hook-form/dist/useForm';
+import {
+  FieldValues,
+  Path,
+  UseFormRegister,
+  useForm,
+  SubmitHandler,
+  SubmitErrorHandler,
+} from 'react-hook-form';
+import styled from 'styled-components';
 
-export interface IInputs {
-  key: string;
+interface IOnClicks {
+  [key: string]: () => void;
+}
+
+export interface IInputs<FormData> {
+  name: Path<FormData & FieldValues>;
   label: string;
   type: 'text' | 'password';
-  isButton: boolean;
+  buttonText?: string;
 }
 
-interface IUseCustomFormParameter {
-  inputs: IInputs[];
-}
+export function useCustomForm<FormData>(
+  inputs: IInputs<FormData>[],
+  onValid: SubmitHandler<FormData & FieldValues>,
+  inValidCustomFn: (errMessage?: string) => void,
+  onClicks?: IOnClicks
+) {
+  const { register, watch, handleSubmit, setValue } = useForm<
+    FormData & FieldValues
+  >();
 
-interface IInputSetProps<T> {
-  register: UseFormRegister<T>;
-  label: string;
-  key: string;
-  type: 'text' | 'password';
-  isButton: boolean;
-}
+  let inputSetObj: { [name: string]: JSX.Element } = {};
+  const inputSetList: JSX.Element[] = [];
 
-export function useCustomForm<T>({ inputs }: IUseCustomFormParameter) {
-  const { register, watch, handleSubmit, setValue } = useForm();
-
-  const inputComponents: React.ReactNode[] = [];
-
+  // 인풋 컴포넌트 생성
   inputs.forEach((el) => {
-    inputComponents.push(
+    inputSetObj[el.name as string] = (
       <InputSet
         register={register}
         label={el.label}
-        key={el.key}
-        isButton={el.isButton}
+        name={el.name}
+        buttonText={el.buttonText}
         type={el.type}
+        onClick={onClicks && onClicks[el.name as string]}
+      />
+    );
+    inputSetList.push(
+      <InputSet
+        register={register}
+        label={el.label}
+        name={el.name}
+        buttonText={el.buttonText}
+        type={el.type}
+        onClick={onClicks && onClicks[el.name as string]}
       />
     );
   });
+
+  const inValid: SubmitErrorHandler<FormData & FieldValues> = (errors) => {
+    if (errors) {
+      inputs.every((el) => {
+        if (errors[el.name as string]) {
+          if (inValidCustomFn)
+            inValidCustomFn(errors[el.name as string]?.message as string);
+          return false;
+        } else {
+          return true;
+        }
+      });
+    }
+  };
+
+  return {
+    input: inputSetObj,
+    inputList: inputSetList,
+    onSubmit: handleSubmit(onValid, inValid),
+    watch,
+  };
 }
 
-const InputSet = ({
+interface IInputSetProps<FormData> {
+  register: UseFormRegister<FormData & FieldValues>;
+  label: string;
+  name: Path<FormData & FieldValues>;
+  type: 'text' | 'password';
+  buttonText?: string;
+  onClick?: () => void;
+}
+function InputSet<FormData>({
   register,
   label,
-  key,
+  name,
   type,
-  isButton,
-}: IInputSetProps<T>) => {
-  return <div></div>;
-};
+  buttonText,
+  onClick,
+}: IInputSetProps<FormData>) {
+  return (
+    <InputSetContainer>
+      <label htmlFor="">{label}</label>
+      <div>
+        <input
+          type={type}
+          {...register(name, { required: `${label}을 입력해주세요.` })}
+          placeholder={`${label}을 입력해주세요.`}
+        />
+        {buttonText && (
+          <button type="button" onClick={onClick}>
+            {buttonText}
+          </button>
+        )}
+      </div>
+    </InputSetContainer>
+  );
+}
+
+const InputSetContainer = styled.div`
+  width: 100%;
+  > label {
+    display: block;
+    margin-bottom: 10px;
+  }
+  > div {
+    width: 100%;
+    height: 35px;
+    display: flex;
+    justify-content: space-between;
+    gap: 10px;
+    > input {
+      width: 100%;
+      padding: 15px;
+      border-radius: 5px;
+      border: 1px solid #b4acac;
+    }
+    > button {
+      width: 30%;
+      white-space: nowrap;
+      border-radius: 5px;
+      border: none;
+      background-color: #39a6f0;
+      color: white;
+    }
+  }
+`;
